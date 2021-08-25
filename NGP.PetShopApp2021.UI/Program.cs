3c77f26033218471ffa909d5703f3f95baf0f13e
Niko4730 <niko4730@easv365.dk>
2021-08-25 09:41:24 +0200

    Initial commit

---
+++ NGP.PetShopApp2021.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NGP.PetShopApp2021.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
