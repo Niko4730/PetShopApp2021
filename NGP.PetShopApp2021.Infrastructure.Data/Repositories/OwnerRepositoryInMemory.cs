f64d20c3e64382e148791f60ea1fae47bc2e7053
Niko4730 <71375600+Niko4730@users.noreply.github.com>
2021-09-12 22:01:29 +0200

    Create OwnerRepositoryInMemory.cs

---
+++ NGP.PetShopApp2021.Infrastructure.Data/Repositories/OwnerRepositoryInMemory.cs
@@ -0,0 +1,13 @@
+﻿using NGP.PetShopApp2021.Core.Models;
+using NGP.PetShopApp2021.Domain.IRepositories;
+
+namespace NGP.PetShopApp2021.Infrastructure.Data.Repositories
+{
+    public class OwnerRepositoryInMemory : IOwnerRepository
+    {
+        public Owner OwnerById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
